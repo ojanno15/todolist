2dd335dfd4038815fcdfae826452d7f4f8a9d936
unknown <ozan.mahdi@gmail.com>
2021-04-05 14:30:51 +0700

    todolist

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
   ],
-
+  router: {
+    base: '/todolist/'
+  },
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
